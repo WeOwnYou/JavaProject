b03d241f9bde8ef2e4e6ad8b4dba2012a4a68112
WeOwnYou <33870211+WeOwnYou@users.noreply.github.com>
2018-11-22 09:01:02 +0300

    Update ListenerFactory.java

--- ListenerFactory.java
+++ ListenerFactory.java
@@ -38,7 +38,7 @@ public class ListenerFactory {
         public void mouseReleased(MouseEvent mouseEvent) {
             x1 = mouseEvent.getX();
             y1 = mouseEvent.getY();
-            System.out.println(x1 + " " +y1);
+            System.out.println(x1 + " " + y1);
             mouseRel = true;
         }
 
@@ -53,48 +53,27 @@ public class ListenerFactory {
         }
 
         public static int[] start_len_tiltOfShip(int cellSize) {                                //расщет длинны(в клетках) и координат и наклона (не проебать проверку на выход за пределы, количество кораблей, )
+//            System.out.println(mouseRel);
+
             if (mouseRel) {
-                boolean isStartFinded = false, isEndFinded = false;             //x0 и y0 > cellSize
+
                 int res[] = new int[4];
-//            System.out.println(2*cellSize+" "+2*cellSize);
-//            System.out.println(x0+" " +y0);
-                for (int i = 9; i >= 0; i--) {                                  //поиск клеток начала и конца
-                    for (int j = 9; j >= 0; j--) {
-                        if (cellSize + i * cellSize <= x0 && cellSize + j * cellSize <= y0 && !isStartFinded && x0<=cellSize+(i+1)*cellSize && y0<= cellSize + (j+1)*cellSize) {
-//                            System.out.println(cellSize + " ! ");
-//                            System.out.println(cellSize + i*cellSize + " " + x0);
-//                            System.out.println(cellSize + j*cellSize + " " + y0);
-                            res[0] = i;
-                            res[1] = j;
-                            isStartFinded = true;
-                        }
-                        if (cellSize + i * cellSize <= x1 && cellSize + j * cellSize <= y1  && !isEndFinded) {
-                            x1 = i;
-                            y1 = j;
-                            isEndFinded = true;
-                        }
-//                    else {
-//                        System.out.println("Я просрал проверку");
-//                    }
-                        if (isEndFinded && isStartFinded)
-                            break;
-                    }
-                    if (isEndFinded && isStartFinded)
-                        break;
-                }
-//            System.out.println("start: " + res[0] + " " + res[1]);
-//            System.out.println("end: " + x1 + " " + y1);
-                if(0 == x1 - res[0] && 0 == y1-res[1]){
+                float x0 = (float) ListenerFactory.ML.x0/(float) cellSize;
+                float y0 = (float) ListenerFactory.ML.y0/(float) cellSize;
+
+//                res[1] = y0/cellSize;
+                System.out.println(x0 + " " +y0);
+                if (0 == x1 - res[0] && 0 == y1 - res[1]) {
                     res[2] = 0;
                     res[3] = 1;
                     return res;
-                }else{
+                } else {
                     if (0 == x1 - res[0]) {                        //возврат длинны и наклона
                         res[2] = abs(y1 - res[0]);
                         res[3] = -1;
                         return res;
-                    }else{
-                        if(0 == y1-res[1]) {
+                    } else {
+                        if (0 == y1 - res[1]) {
                             res[2] = abs(x1 - res[0]);
                             res[3] = 1;
                             return res;
@@ -102,7 +81,7 @@ public class ListenerFactory {
                     }
                 }
             }
-            int []t = {-1,-1,-1,-1};
+            int[] t = {-1, -1, -1, -1};
             return t;
         }
 
