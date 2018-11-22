e95fc126846945ff82e80b1f3c560f87de62ea43
WeOwnYou <33870211+WeOwnYou@users.noreply.github.com>
2018-11-22 16:41:20 +0300

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -5,15 +5,12 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class Ship {
+public  class Ship {
 
-    private int x0, x1, y0, y1;
+    private static int x0, x1, y0, y1, len;
     private Image shipImg = null;                              //картинку найти
 
-    public Ship (int x0, int y0, int x1, int y1){
-        if (!((x0 == x1) || (y0==y1))){
-
-        }
+    public  Ship (int x0, int y0, int x1, int y1){               //isHorizontal
         this.x0 = x0;
         this.y0 = y0;
         this.x1 = x1;
@@ -27,5 +24,21 @@ public class Ship {
 
     public Image getShipImg() {
         return shipImg;
+    }
+
+    public int getY0() {
+        return y0;
+    }
+
+    public int getX0() {
+        return x0;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
     }
 }
