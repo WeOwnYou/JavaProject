409bbb6fbed45b7b1e27808bff99ba4d7272ac83
WeOwnYou <33870211+WeOwnYou@users.noreply.github.com>
2018-11-23 16:58:22 +0300

    Update ListenerFactory.java

--- ListenerFactory.java
+++ ListenerFactory.java
@@ -14,10 +14,9 @@ public class ListenerFactory {
 
     public static class ML implements MouseListener {
 
-        private static boolean mouseRel = false;
-        private static int x0, y0, x1, y1;
+        private  static int x0, y0, x1, y1;
+        private boolean isHorizontal;
         private Game game;
-//        private static int cellSize;
 
 
         public ML(Game game) {
@@ -30,26 +29,32 @@ public class ListenerFactory {
 
         @Override
         public void mousePressed(MouseEvent mouseEvent) {
-            x0 = mouseEvent.getX() - 10;     //фикшу курсов
+            x0 = mouseEvent.getX() - 10;     //эксепты для неправльного ввода
             y0 = mouseEvent.getY() - 30;
-            mouseRel = false;
         }
 
         @Override
         public void mouseReleased(MouseEvent mouseEvent) {
             x1 = mouseEvent.getX() - 10;
             y1 = mouseEvent.getY() - 30;
-            System.out.println(x1 + " " + y1);
-            mouseRel = true;
+//            System.out.println(x1 + " " + y1);
 
             int cellSize = this.game.countCellSize();
-            int x0 = (int)((float) ListenerFactory.ML.x0/(float) cellSize) - 1;
-            int y0 = (int)((float) ListenerFactory.ML.y0/(float) cellSize) - 1;
-            int x1 = (int)((float) ListenerFactory.ML.x1/(float) cellSize) - 1;
-            int y1 = (int)((float) ListenerFactory.ML.y1/(float) cellSize) - 1;
-            this.game.createShip(x0,y0,x1,y1);
-            System.out.println(x0 + " " + y0 + " " + x1 + " " + y1);
+            int x0 = (int) ((float) ListenerFactory.ML.x0 / (float) cellSize) - 1;
+            int y0 = (int) ((float) ListenerFactory.ML.y0 / (float) cellSize) - 1;
+            int x1 = (int) ((float) ListenerFactory.ML.x1 / (float) cellSize) - 1;
+            int y1 = (int) ((float) ListenerFactory.ML.y1 / (float) cellSize) - 1;
+            if (x1 - x0 == 0) {
+                isHorizontal = false;
+                this.game.createShip(x0, y0, x1, y1, isHorizontal);
+            } else {
+                if (y1 - y0 == 0) {
+                    isHorizontal = true;
+                    this.game.createShip(x0, y0, x1, y1, isHorizontal);
+                }
+//            System.out.println(x0 + " " + y0 + " " + x1 + " " + y1);
 
+            }
         }
 
         @Override
