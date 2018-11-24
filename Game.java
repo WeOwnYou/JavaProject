096b826e0f5c98224def36a5c05e24867c8ecec6
WeOwnYou <33870211+WeOwnYou@users.noreply.github.com>
2018-11-24 09:23:46 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,28 +1,26 @@
 package com.company;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Game extends JPanel {
 
-    public Image img = null;
     private int gameField[][];
     private ArrayList<Ship> Ships;
-    private boolean running;
     private MainWindow1 mv;
     private int numberOfShips[] = {0, 4 , 3, 2, 1};
 
     public int countCellSize() {                                                        //установка размеров клеток
-        int cellSize = 0;
+        int cellSize;
         if (mv.getWidth() <= mv.getHeight()) {
-            cellSize = mv.getWidth() / 13;
+            cellSize = (int)(mv.getWidth())/ 13;                        //сделать зависимость от процентов
             cellSize = (mv.getWidth() - 2 * cellSize) / 11;
         } else {
-            cellSize = mv.getHeight() / 13;
+            cellSize = (int)(mv.getHeight()) / 13;
             cellSize = (mv.getHeight() - 2 * cellSize) / 11;
         }
         return cellSize;
@@ -32,59 +30,62 @@ public class Game extends JPanel {
 
         gameField = new int[10][10];
         Ships = new ArrayList<Ship>(10);
-        running = true;
         this.mv = mv;
         mv.addMouseListener(ListenerFactory.ML.getMouseListener((ListenerFactory.MOUSE_LISTENER), this));
 
     }
 
     public void createShip(int x0, int y0, int x1, int y1, boolean isHorizpntal) {                                        //запихивание информации о корабле в корбаль
-        boolean isAbleToPlace = true;                                                                                                                    //в теории можно убрать один из циклов
+        boolean isAbleToPlace = true;
+        boolean isAbleOfSq = true;
+        int len = Math.max(Math.abs(x1-x0)+1,Math.abs(y1-y0)+1);
+        if(len > 4){
+            System.out.println("na ekran");
+            return;
+        }
+        if(numberOfShips[len] > 0 ) {
+            isAbleOfSq = true;
+        }
+        else{
+            isAbleOfSq = false;
+        }
         if(isHorizpntal) {
-            System.out.println(x0 + " " + y0 + " " + x1 + " " + y1);
-            for (int i = x0 - 1; i <= x1 + 1; i++) {
+            for (int i = x0; i <= x1; i++)
+                if (gameField[y0][i] == 1) {
+                    isAbleToPlace = false;
+                    break;
+                }
+            for (int i = x0 - 1; i <= x1 + 1 && isAbleOfSq && isAbleToPlace; i++)
                 for (int j = y0 - 1; j <= y1 + 1; j++) {
-                    if(i>=x0 && i<=x1 && j>=y0 && j<=y1)
-                        if(gameField[j][i] == 1) {
-                            isAbleToPlace = false;
-                            break;
-                        }
                     try {
                         gameField[j][i] = 1;
                     } catch (IndexOutOfBoundsException e) {
                         ;
                     }
                 }
-                if(!isAbleToPlace)
+        }else{
+            for (int i = y0; i <= y1; i++)
+                if (gameField[i][x0] == 1) {
+                    isAbleToPlace = false;
                     break;
-            }
-        }
-        else {
-            for (int i = y0 - 1; i <= y1 + 1; i++) {
+                }
+            for (int i = y0 - 1; i <= y1 + 1 && isAbleOfSq && isAbleToPlace; i++)
                 for (int j = x0 - 1; j <= x1 + 1; j++) {
-                    if(j>=x0 && j<=x1 && i>=y0 && i<=y1 )
-                        if(gameField[i][j] == 1) {
-                            isAbleToPlace = false;
-                            break;
-                        }
                     try {
                         gameField[i][j] = 1;
                     } catch (IndexOutOfBoundsException e) {
-                        ;;
+                        ;
                     }
                 }
-                if(!isAbleToPlace)
-                    break;
-            }
         }
-        if(isAbleToPlace) {
+
+        if(isAbleOfSq && isAbleToPlace) {
+            numberOfShips[len] -= 1;
             Ship s = new Ship(x0, y0, x1, y1, isHorizpntal);
             Ships.add(s);
             repaint();
         }
-        else
-            System.out.println("Dodik");                                                                        //na ekran
-        if(isAbleToPlace) {
+        if(isAbleToPlace ) {
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++)
                     System.out.print(gameField[i][j] + " ");
@@ -132,11 +133,17 @@ public class Game extends JPanel {
 //                    lenx*cellSize, leny*cellSize,null);
         }
     }
-    public void paintShip(Graphics g, Image img, int x, int y, int width, int height, boolean isHorizontal){
-        Graphics2D g2d=(Graphics2D)g;
-//        System.out.println(isHorizontal);
-        if(!isHorizontal)
-            g2d.rotate(Math.PI/2);
-        g2d.drawImage(img, x, y, width, height, null);
+    public void paintShip(Graphics g, Image img, int x, int y, int width, int height, boolean isHorizontal) {
+        Graphics2D g2d = (Graphics2D) g;
+        if (!isHorizontal) {
+            double rotationRequired = Math.PI / 2;
+            double locationX = img.getWidth(null)/4.;
+            double locationY = img.getHeight(null)/4.;
+            AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
+            AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+            g2d.drawImage(op.filter((BufferedImage) img, null), x, y, width, height, null);
+        } else {
+            g2d.drawImage(img, x, y, width, height, null);
+        }
     }
 }
